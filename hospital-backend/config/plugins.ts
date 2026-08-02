a4bfb9f49c7710f0bd1bd38cbca5285ba042a686
mukesh <mukeshkalkute4@gmail.com>
2026-08-02 19:13:56 +0530

    Switch SMTP to Gmail

--- hospital-backend/config/plugins.ts
+++ hospital-backend/config/plugins.ts
@@ -27,23 +27,24 @@ const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Plugin =>
   config: {},
 },
 
-  email: {
-    config: {
-      provider: 'nodemailer',
-      providerOptions: {
-        host: env('SMTP_HOST'),
-        port: env.int('SMTP_PORT'),
-        auth: {
-          user: env('SMTP_USER'),
-          pass: env('SMTP_PASS'),
-        },
-      },
-      settings: {
-        defaultFrom: env('SMTP_FROM'),
-        defaultReplyTo: env('SMTP_FROM'),
+ email: {
+  config: {
+    provider: "nodemailer",
+    providerOptions: {
+      host: env("SMTP_HOST"),
+      port: env.int("SMTP_PORT"),
+      secure: false,
+      auth: {
+        user: env("SMTP_USER"),
+        pass: env("SMTP_PASS"),
       },
     },
+    settings: {
+      defaultFrom: env("SMTP_FROM"),
+      defaultReplyTo: env("SMTP_FROM"),
+    },
   },
+},
 
   upload: {
     config: {
